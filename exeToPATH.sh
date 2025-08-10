ca2ebac01979c4b2fb8c56696a2a7fc2b3da15d3
Drew Osmundson <drewosmundson@gmail.com>
2025-08-10 14:01:20 -0500

    fixing syntax

--- exeToPATH.sh
+++ exeToPATH.sh
@@ -3,7 +3,7 @@
 
 
 help() { 
-    echo "Usage: $./exeToPATH (Options) (/path/to/file)"
+    echo "Usage: $./exeToPATH (Options) (/path/to/exe)"
 
     echo "./exeToPATH"
     echo "  The name of this script. Ensure that it has the correct permissions so that it is able to execute. "
@@ -18,66 +18,71 @@ help() {
     echo "  -i, -install //adds the path to the specified executable to the PATH variable. If none specified promt user"
 
     echo "  -u, -uninstall //unistalls the script if no executable specified prompt the user"
-            
-    echo "/path/to/file:"
-    echo "  The executable file to be added to the PATH"
-}
-check(FILE) { 
-  if [ t ]
 
+    echo "/path/to/exe:"
+    echo "  The executable exe to be added to the PATH"
+}
+check() { 
+  if [ type -P $exe ]; then 
+    return true
+  else
+    return false
+  fi
 }
 
 install() {
-
+  if [ check $exe -eq true ]; then
+    uninstall $exe
+  fi
 }
 
 uninstall() {
-
+  if [ check $exe -eq false ]; then
+    return true;
+  fi
 }
 
 
-
-
-
 main(){
-  #check for parameters if none then promt for file path and list the help command
-  if [ $# -lt 2] then 
+  #check for parameters if none then promt for exe path and list the help command
+  if [ "$#" -lt 1 ]; then 
     echo "Enter the path to the executable you would like to install in your PATH"
-  else
-    echo "No executable argument usage: $./exeToPATH -options path/to/your/exe"
-    echo "Use ./exeToPATH help for a list of commands"
-    exit 1
+    echo "Usage: $./exeToPATH path/to/your/exe -options"
+    echo "For a list of commands type $./exeToPATH help"
+    exit 0
   fi
   
-  if [ $1 -eq "help" ] || [ $2 -eq "help" ] then
+  if [ $1 -eq "help" ] || [ $2 -eq "help" ]; then
     help
   fi
 
-  if [ -x $1 ] then
-    FILE=$1
+  if [ -x $1 ]; then
+    exe=$1
   else 
-    echo "Specified file is not an executable."
-    echo "Make sure your file has the right permissions and has no complation errors"
+    echo "Specified exe is not an executable."F
+    echo "Make sure your exe has the right permissions and has no complation errors"
     exit 1
   fi
 
-  #check if the file is already installed in the system
-  if [ $2 -eq "-c"] || [ $2 -eq "-check"] then
-    check(FILE)
-  
-  elif [ $2 -eq "-i"] || [ $2 -eq "-u"] then
-    uninstall
 
-  elif [ $2 -eq "-u"] || [ $2 -eq "-u"] then
-    install
+  if [ $2 -eq "-c"] || [ $2 -eq "-check"]; then
+    if [ check $exe -eq true ]; then
+      echo "This executable is in  PATH"
+    else
+      echo "this executable is not in PATH"
+    fi
 
-  else 
+  elif [ $2 -eq "-i"] || [ $2 -eq "-install"]; then
+    install $exe
+
+  elif [ $2 -eq "-u"] || [ $2 -eq "-uninstall"]; then
+    uninstall $exe
+
+  fi
     
 }
 
 
-
-echo "Script running"
 main
 echo "Script exiting normally"
 exit 0
